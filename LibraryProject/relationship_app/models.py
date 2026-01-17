adac748eb8f0996f6d4dd35265bfab6d87881e8d
Zubaida <zubaidasuleiman89@gmail.com>
2026-01-17 13:11:25 +0100

    update

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -17,6 +17,13 @@ class Book(models.Model):
         on_delete=models.CASCADE,
         related_name='books'
     )
+     class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
 
     def __str__(self):
         return self.title
--- LibraryProject/relationship_app/urls.py
+++ LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -8,4 +9,7 @@ urlpatterns = [
     path('admin-role/', admin_view, name='admin_view'),
     path('librarian-role/', librarian_view, name='librarian_view'),
     path('member-role/', member_view, name='member_view'),
+    path('book/add/', add_book, name='add_book'),
+    path('book/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('book/delete/<int:pk>/', delete_book, name='delete_book'),
 ]
