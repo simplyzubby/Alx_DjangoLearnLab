54f8e6dbb242e98ee85659abffcd8636d475f734
Zubaida <zubaidasuleiman89@gmail.com>
2026-01-17 12:17:14 +0100

    update

--- LibraryProject/LibraryProject/urls.py
+++ LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
+    path('admin/', admin.site.urls),
 ]
+
+
