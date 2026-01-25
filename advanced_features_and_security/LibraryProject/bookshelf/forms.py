85342c38c0ca15a0bdeb62d06e0001435f081f70
Zubaida <zubaidasuleiman89@gmail.com>
2026-01-25 21:35:52 +0100

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100)
 class SearchForm(forms.Form):
     q = forms.CharField(
         max_length=100,
