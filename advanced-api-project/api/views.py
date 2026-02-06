4c8b08471b4ef63662662baeb0faf8cc3062587f
Zubaida <zubaidasuleiman89@gmail.com>
2026-02-06 23:01:16 +0100

    update

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 # ------------------------------
